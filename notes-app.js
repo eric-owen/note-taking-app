fd73796a1c9e23ac1208f1c6d86d2dae3ee8588c
eric-owen <65836532+eric-owen@users.noreply.github.com>
2020-10-11 00:41:58 -0400

    refactored functions

--- notes-app.js
+++ notes-app.js
@@ -1,49 +1,28 @@
-const notes = [{
-    title: 'next trip',
-    body: 'go to spain'
-}, {
-    title: 'habbits to obtain',
-    body: 'exercise, eat better'
-
-}, {
-    title: 'office mods',
-    body: 'new seat'
-
-}]
+const notes = getSavedNotes()
 
 const filters = {
     searchText: ''
-}
-
-const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-    
-    document.querySelector('#notes').innerHTML = ''
-
-    filteredNotes.forEach(function (note) {
-        const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
-        document.querySelector('#notes').appendChild(noteEl)
-
-    })
 }
 
 renderNotes(notes, filters)
 
 
 document.querySelector('#create-note').addEventListener('click', function () {
-    console.log('did this work')
+    notes.push({
+        title: '',
+        body: ''
+    })
+
+    saveNotes(notes)
+    renderNotes(notes, filters)
 })
 
-document.querySelector('#remove-all').addEventListener('click', function (e) {
-    document.querySelectorAll('.note').forEach(function (note) {
-        note.remove()
-    })
-})
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    e.target.value
 })
